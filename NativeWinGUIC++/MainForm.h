73e776ad6dd32423fe92da5ede15eb96480525d0
Samuel Morales Valbuena <samuelmv512@gmail.com>
2025-06-03 14:18:28 -0500

    Added multiple labels for available compounds, arranged them and changed project name

--- NativeWinGUIC++/MainForm.h
+++ NativeWinGUIC++/MainForm.h
@@ -91,8 +91,41 @@ namespace NativeWinGUIC {
 	private: System::Windows::Forms::RadioButton^ rdbLogDEBUG;
 	private: System::Windows::Forms::Panel^ panel1;
 	private: System::Windows::Forms::GroupBox^ groupBox4;
-	private: System::Windows::Forms::Label^ lblAvailableCompounds;
+
 	private: System::Windows::Forms::Button^ btnHelp;
+
+
+
+	private: System::Windows::Forms::Label^ label16;
+	private: System::Windows::Forms::Label^ label15;
+
+
+
+	private: System::Windows::Forms::Label^ label9;
+	private: System::Windows::Forms::Label^ label8;
+
+
+
+
+	private: System::Windows::Forms::Label^ label22;
+	private: System::Windows::Forms::Label^ label21;
+
+
+	private: System::Windows::Forms::Label^ label26;
+
+	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel1;
+	private: System::Windows::Forms::Label^ label18;
+	private: System::Windows::Forms::Label^ label17;
+	private: System::Windows::Forms::Label^ label24;
+	private: System::Windows::Forms::Label^ label25;
+	private: System::Windows::Forms::Label^ label23;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Label^ label12;
+	private: System::Windows::Forms::Label^ label20;
+	private: System::Windows::Forms::Label^ label13;
+	private: System::Windows::Forms::Label^ label19;
+	private: System::Windows::Forms::Label^ label14;
 
 
 
@@ -149,13 +182,33 @@ namespace NativeWinGUIC {
 			this->rdbLogFULL = (gcnew System::Windows::Forms::RadioButton());
 			this->groupBox3 = (gcnew System::Windows::Forms::GroupBox());
 			this->groupBox4 = (gcnew System::Windows::Forms::GroupBox());
-			this->lblAvailableCompounds = (gcnew System::Windows::Forms::Label());
+			this->tableLayoutPanel1 = (gcnew System::Windows::Forms::TableLayoutPanel());
+			this->label18 = (gcnew System::Windows::Forms::Label());
+			this->label26 = (gcnew System::Windows::Forms::Label());
+			this->label17 = (gcnew System::Windows::Forms::Label());
+			this->label24 = (gcnew System::Windows::Forms::Label());
+			this->label25 = (gcnew System::Windows::Forms::Label());
+			this->label23 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label8 = (gcnew System::Windows::Forms::Label());
+			this->label22 = (gcnew System::Windows::Forms::Label());
+			this->label9 = (gcnew System::Windows::Forms::Label());
+			this->label21 = (gcnew System::Windows::Forms::Label());
+			this->label12 = (gcnew System::Windows::Forms::Label());
+			this->label20 = (gcnew System::Windows::Forms::Label());
+			this->label13 = (gcnew System::Windows::Forms::Label());
+			this->label19 = (gcnew System::Windows::Forms::Label());
+			this->label14 = (gcnew System::Windows::Forms::Label());
+			this->label15 = (gcnew System::Windows::Forms::Label());
+			this->label16 = (gcnew System::Windows::Forms::Label());
 			this->btnHelp = (gcnew System::Windows::Forms::Button());
 			this->groupBox1->SuspendLayout();
 			this->groupBox2->SuspendLayout();
 			this->panel1->SuspendLayout();
 			this->groupBox3->SuspendLayout();
 			this->groupBox4->SuspendLayout();
+			this->tableLayoutPanel1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// label1
@@ -458,7 +511,7 @@ namespace NativeWinGUIC {
 			// 
 			// groupBox4
 			// 
-			this->groupBox4->Controls->Add(this->lblAvailableCompounds);
+			this->groupBox4->Controls->Add(this->tableLayoutPanel1);
 			this->groupBox4->Location = System::Drawing::Point(405, 145);
 			this->groupBox4->Name = L"groupBox4";
 			this->groupBox4->Size = System::Drawing::Size(297, 152);
@@ -466,14 +519,222 @@ namespace NativeWinGUIC {
 			this->groupBox4->TabStop = false;
 			this->groupBox4->Text = L"Available Compounds";
 			// 
-			// lblAvailableCompounds
+			// tableLayoutPanel1
 			// 
-			this->lblAvailableCompounds->Location = System::Drawing::Point(9, 25);
-			this->lblAvailableCompounds->Name = L"lblAvailableCompounds";
-			this->lblAvailableCompounds->Size = System::Drawing::Size(282, 110);
-			this->lblAvailableCompounds->TabIndex = 0;
-			this->lblAvailableCompounds->Text = L"O2 H H2 OH H2O N N2 NO CO CO2 CH4 C2H2 Cl Cl2 HCl NH NH3 CH2 NO2";
-			this->lblAvailableCompounds->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			this->tableLayoutPanel1->ColumnCount = 8;
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle()));
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle()));
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle()));
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle()));
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle()));
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle()));
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Absolute,
+				56)));
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Absolute,
+				89)));
+			this->tableLayoutPanel1->Controls->Add(this->label18, 0, 0);
+			this->tableLayoutPanel1->Controls->Add(this->label26, 6, 5);
+			this->tableLayoutPanel1->Controls->Add(this->label17, 0, 4);
+			this->tableLayoutPanel1->Controls->Add(this->label24, 5, 5);
+			this->tableLayoutPanel1->Controls->Add(this->label25, 0, 5);
+			this->tableLayoutPanel1->Controls->Add(this->label23, 1, 5);
+			this->tableLayoutPanel1->Controls->Add(this->label2, 1, 0);
+			this->tableLayoutPanel1->Controls->Add(this->label5, 5, 0);
+			this->tableLayoutPanel1->Controls->Add(this->label8, 6, 0);
+			this->tableLayoutPanel1->Controls->Add(this->label22, 7, 4);
+			this->tableLayoutPanel1->Controls->Add(this->label9, 7, 0);
+			this->tableLayoutPanel1->Controls->Add(this->label21, 6, 4);
+			this->tableLayoutPanel1->Controls->Add(this->label12, 0, 3);
+			this->tableLayoutPanel1->Controls->Add(this->label20, 5, 4);
+			this->tableLayoutPanel1->Controls->Add(this->label13, 1, 3);
+			this->tableLayoutPanel1->Controls->Add(this->label19, 1, 4);
+			this->tableLayoutPanel1->Controls->Add(this->label14, 5, 3);
+			this->tableLayoutPanel1->Controls->Add(this->label15, 6, 3);
+			this->tableLayoutPanel1->Controls->Add(this->label16, 7, 3);
+			this->tableLayoutPanel1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->tableLayoutPanel1->Location = System::Drawing::Point(27, 19);
+			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
+			this->tableLayoutPanel1->RowCount = 6;
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle()));
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle()));
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle()));
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle()));
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 18)));
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 22)));
+			this->tableLayoutPanel1->Size = System::Drawing::Size(244, 116);
+			this->tableLayoutPanel1->TabIndex = 20;
+			// 
+			// label18
+			// 
+			this->label18->AutoSize = true;
+			this->label18->Location = System::Drawing::Point(3, 0);
+			this->label18->Name = L"label18";
+			this->label18->Size = System::Drawing::Size(35, 17);
+			this->label18->TabIndex = 11;
+			this->label18->Text = L"CH4";
+			// 
+			// label26
+			// 
+			this->label26->AutoSize = true;
+			this->label26->Location = System::Drawing::Point(137, 52);
+			this->label26->Name = L"label26";
+			this->label26->Size = System::Drawing::Size(37, 17);
+			this->label26->TabIndex = 19;
+			this->label26->Text = L"NO2";
+			// 
+			// label17
+			// 
+			this->label17->AutoSize = true;
+			this->label17->Location = System::Drawing::Point(3, 34);
+			this->label17->Name = L"label17";
+			this->label17->Size = System::Drawing::Size(36, 17);
+			this->label17->TabIndex = 10;
+			this->label17->Text = L"CO2";
+			// 
+			// label24
+			// 
+			this->label24->AutoSize = true;
+			this->label24->Location = System::Drawing::Point(95, 52);
+			this->label24->Name = L"label24";
+			this->label24->Size = System::Drawing::Size(36, 17);
+			this->label24->TabIndex = 17;
+			this->label24->Text = L"NH3";
+			// 
+			// label25
+			// 
+			this->label25->AutoSize = true;
+			this->label25->Location = System::Drawing::Point(3, 52);
+			this->label25->Name = L"label25";
+			this->label25->Size = System::Drawing::Size(35, 17);
+			this->label25->TabIndex = 18;
+			this->label25->Text = L"CH2";
+			// 
+			// label23
+			// 
+			this->label23->AutoSize = true;
+			this->label23->Location = System::Drawing::Point(46, 52);
+			this->label23->Name = L"label23";
+			this->label23->Size = System::Drawing::Size(28, 17);
+			this->label23->TabIndex = 16;
+			this->label23->Text = L"NH";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(46, 0);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(27, 17);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"O2";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(95, 0);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(18, 17);
+			this->label5->TabIndex = 2;
+			this->label5->Text = L"H";
+			// 
+			// label8
+			// 
+			this->label8->AutoSize = true;
+			this->label8->Location = System::Drawing::Point(137, 0);
+			this->label8->Name = L"label8";
+			this->label8->Size = System::Drawing::Size(26, 17);
+			this->label8->TabIndex = 3;
+			this->label8->Text = L"H2";
+			// 
+			// label22
+			// 
+			this->label22->AutoSize = true;
+			this->label22->Location = System::Drawing::Point(193, 34);
+			this->label22->Name = L"label22";
+			this->label22->Size = System::Drawing::Size(30, 17);
+			this->label22->TabIndex = 15;
+			this->label22->Text = L"HCl";
+			// 
+			// label9
+			// 
+			this->label9->AutoSize = true;
+			this->label9->Location = System::Drawing::Point(193, 0);
+			this->label9->Name = L"label9";
+			this->label9->Size = System::Drawing::Size(29, 17);
+			this->label9->TabIndex = 4;
+			this->label9->Text = L"OH";
+			// 
+			// label21
+			// 
+			this->label21->AutoSize = true;
+			this->label21->Location = System::Drawing::Point(137, 34);
+			this->label21->Name = L"label21";
+			this->label21->Size = System::Drawing::Size(28, 17);
+			this->label21->TabIndex = 14;
+			this->label21->Text = L"Cl2";
+			// 
+			// label12
+			// 
+			this->label12->AutoSize = true;
+			this->label12->Location = System::Drawing::Point(3, 17);
+			this->label12->Name = L"label12";
+			this->label12->Size = System::Drawing::Size(37, 17);
+			this->label12->TabIndex = 5;
+			this->label12->Text = L"H2O";
+			// 
+			// label20
+			// 
+			this->label20->AutoSize = true;
+			this->label20->Location = System::Drawing::Point(95, 34);
+			this->label20->Name = L"label20";
+			this->label20->Size = System::Drawing::Size(20, 17);
+			this->label20->TabIndex = 13;
+			this->label20->Text = L"Cl";
+			// 
+			// label13
+			// 
+			this->label13->AutoSize = true;
+			this->label13->Location = System::Drawing::Point(46, 17);
+			this->label13->Name = L"label13";
+			this->label13->Size = System::Drawing::Size(18, 17);
+			this->label13->TabIndex = 6;
+			this->label13->Text = L"N";
+			// 
+			// label19
+			// 
+			this->label19->AutoSize = true;
+			this->label19->Location = System::Drawing::Point(46, 34);
+			this->label19->Name = L"label19";
+			this->label19->Size = System::Drawing::Size(43, 17);
+			this->label19->TabIndex = 12;
+			this->label19->Text = L"C2H2";
+			// 
+			// label14
+			// 
+			this->label14->AutoSize = true;
+			this->label14->Location = System::Drawing::Point(95, 17);
+			this->label14->Name = L"label14";
+			this->label14->Size = System::Drawing::Size(26, 17);
+			this->label14->TabIndex = 7;
+			this->label14->Text = L"N2";
+			// 
+			// label15
+			// 
+			this->label15->AutoSize = true;
+			this->label15->Location = System::Drawing::Point(137, 17);
+			this->label15->Name = L"label15";
+			this->label15->Size = System::Drawing::Size(29, 17);
+			this->label15->TabIndex = 8;
+			this->label15->Text = L"NO";
+			// 
+			// label16
+			// 
+			this->label16->AutoSize = true;
+			this->label16->Location = System::Drawing::Point(193, 17);
+			this->label16->Name = L"label16";
+			this->label16->Size = System::Drawing::Size(28, 17);
+			this->label16->TabIndex = 9;
+			this->label16->Text = L"CO";
 			// 
 			// btnHelp
 			// 
@@ -503,7 +764,7 @@ namespace NativeWinGUIC {
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->MaximizeBox = false;
 			this->Name = L"MainForm";
-			this->Text = L"AdiabaticFlameC++";
+			this->Text = L"Adiabatic Flame Temperature Calculator";
 			this->Load += gcnew System::EventHandler(this, &MainForm::MainForm_Load);
 			this->groupBox1->ResumeLayout(false);
 			this->groupBox1->PerformLayout();
@@ -514,6 +775,8 @@ namespace NativeWinGUIC {
 			this->groupBox3->ResumeLayout(false);
 			this->groupBox3->PerformLayout();
 			this->groupBox4->ResumeLayout(false);
+			this->tableLayoutPanel1->ResumeLayout(false);
+			this->tableLayoutPanel1->PerformLayout();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -584,5 +847,8 @@ namespace NativeWinGUIC {
 	private: System::Void btnRun_Click(System::Object^ sender, System::EventArgs^ e);
 
 	private: System::Void btnHelp_Click(System::Object^ sender, System::EventArgs^ e);
+
+
+
 };
 }
